4a4b4aa5de27d8535d59958077801df4c36aceeb
MingyiLiu <liumy@hit.edu.cn>
2019-12-14 20:46:13 +0800

    update

--- convert.py
+++ convert.py
@@ -3,14 +3,19 @@ import json
 QUOTATIONS = ["\"", "「", "“", "‘", "’", "”"]
 
 
-def convert_single_line(sample) -> tuple:
+def convert_single_line(sample, predict=True) -> tuple:
     """Convert a single line into a subgraph.
     Args:
         sample: A json string.
     Returns:
         tuple: (nodes, edges)
     """
-    text, labels, relation = sample['text'], sample['labels'], sample['relation']
+    if predict:
+        text, labels, relation = sample['text'], sample['labels'], sample['relation']
+    else:
+        text, labels = sample['text'], sample['labels']
+        labels = sorted(labels, key=lambda x: x[0], reverse=True)
+        labels, relation = labels[:-1], labels[-1][2]
     timestamp = text.split(',')[-1].strip()
     labels = sorted(labels, key=lambda x: x[0])
     nodes, edges = [], []
@@ -20,15 +25,18 @@ def convert_single_line(sample) -> tuple:
         if label_i[2] == 'Object' and label_j[2] in ['Recipient', 'Actor']:
             if label_i[1] == label_j[0] or (label_i[1]+1 == label_j[0] and text[label_i[1]] in QUOTATIONS) :
                 feature_id, stakeholder_id = text[label_i[0]: label_i[1]], text[label_j[0]: label_j[1]]
-                nodes.append((feature_id, {"node_type": "service_feature"}))
+                nodes.append((feature_id, {"node_type": "feature"}))
                 nodes.append((stakeholder_id, {"node_type": "stakeholder"}))
                 edges.append((stakeholder_id, feature_id, {"edge_type": "provide", "created_at": timestamp, "text": text}))
                 merged_labels.append(label_i)
+        # TODO: How to handle Attribute more properly? Now I simply remove it.
+        if label_i[2] == 'Attribute':
+            merged_labels.append(label_i)
     for label in merged_labels:
         labels.remove(label)
     # ==================================================================================================================
 
-    if relation == "non":
+    if relation == "non" or relation == "res":
         comma_index = text.index(',')
         label_1, label_2 = [], []
         for label in labels:
@@ -36,9 +44,13 @@ def convert_single_line(sample) -> tuple:
                 label_1.append(list(label))
             else:
                 label_2.append(list(label))
-        labels = [label_1, label_2]
+        if relation == "non":
+            labels = [label_1, label_2]
+        else:
+            labels = [label_2 + label_1]
     else:
         labels = [list(labels)]
+
     flags = 0
     for labels_item in labels:
         # Rule 2: If less than one entity ignores
@@ -70,5 +82,54 @@ def convert_single_line(sample) -> tuple:
             nodes.append((feature, {"node_type": "feature"}))
             edges.append((stakeholder, feature, {"edge_type": action, "created_at": timestamp, "text": text}))
             continue
+
+        # Rule5
+        if relation == "Ori" and '-'.join([label[2] for label in labels_item]) == "Actor-Action-Object-Action-Object":
+            flags += 1
+            stakeholder, action1, feature1, action2, feature2 = tuple(labels_item)
+            stakeholder = text[stakeholder[0]: stakeholder[1]]
+            action1, action2 = text[action1[0]: action1[1]], text[action2[0]: action2[1]]
+            feature1, feature2 = text[feature1[0]: feature1[1]], text[feature2[0]: feature2[1]]
+            nodes.append((stakeholder, {"node_type": "stakeholder"}))
+            nodes.append((feature1, {"node_type": "feature"}))
+            nodes.append((feature2, {"node_type": "feature"}))
+            edges.append((stakeholder, feature1, {"edge_type": action1, "created_at": timestamp, "text": text}))
+            edges.append((stakeholder, feature2, {"edge_type": action2, "created_at": timestamp, "text": text}))
+            continue
+
+        # Rule6:
+        if relation == "Ori" and '-'.join([label[2] for label in labels_item]) == "Actor-Action-Recipient-Object-Action-Object":
+            flags += 1
+            stakeholder, action1, stakeholder2,feature1, action2, feature2 = tuple(labels_item)
+            stakeholder = text[stakeholder[0]: stakeholder[1]]
+            stakeholder2 = text[stakeholder2[0]: stakeholder2[1]]
+            action1, action2 = text[action1[0]: action1[1]], text[action2[0]: action2[1]]
+            feature1, feature2 = text[feature1[0]: feature1[1]], text[feature2[0]: feature2[1]]
+            nodes.append((stakeholder, {"node_type": "stakeholder"}))
+            nodes.append((stakeholder2, {"node_type": "stakeholder"}))
+            nodes.append((feature1, {"node_type": "feature"}))
+            nodes.append((feature2, {"node_type": "feature"}))
+            edges.append((stakeholder, feature1, {"edge_type": action1, "created_at": timestamp, "text": text}))
+            edges.append((stakeholder, feature2, {"edge_type": action2, "created_at": timestamp, "text": text}))
+            edges.append((stakeholder, stakeholder2, {"edge_type": action1, "created_at": timestamp, "text": text}))
+            edges.append((stakeholder2, feature1, {"edge_type": action1, "created_at": timestamp, "text": text}))
+            edges.append((stakeholder2, feature2, {"edge_type": action2, "created_at": timestamp, "text": text}))
+            continue
+
+        # TODO: Other Format
+        pattern = '-'.join([label[2] for label in labels_item])
+        stakeholders, features = [], []
+        for label in labels_item:
+            if label[2] == 'Actor' or label[2] == 'Recipient':
+                stakeholders.append(text[label[0]: label[1]])
+                nodes.append((text[label[0]: label[1]], {"node_type": "stakeholder"}))
+            elif label[2] == 'Object':
+                features.append(text[label[0]: label[1]])
+                nodes.append((text[label[0]: label[1]], {"node_type": "feature"}))
+        for index, stakeholder in enumerate(stakeholders[:-1]):
+            for node2 in stakeholders[index + 1:]:
+                edges.append((stakeholder, node2, {"edge_type": "uf1", "created_at": timestamp, "text": text, "pattern": pattern}))
+            for node2 in features:
+                edges.append((stakeholder, node2, {"edge_type": "uf2", "created_at": timestamp, "text": text, "pattern": pattern}))
         # =============================================================================================================
-    return nodes, edges, len(labels) == flags
+    return nodes, edges
